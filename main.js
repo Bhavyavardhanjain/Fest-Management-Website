0266b43d3079ef1a3f896bae5f2c9c6af801ece7
Bhavya Vardhan Jain <67510492+Bhavyavardhanjain@users.noreply.github.com>
2023-04-01 11:11:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var faq = document.getElementsByClassName("ques");
 var i;
-
 for (i = 0; i < faq.length; i++) {
     faq[i].addEventListener("click", function () {
         this.classList.toggle("active");
