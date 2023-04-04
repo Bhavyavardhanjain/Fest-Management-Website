0a08fdf3f2eb4c00ae2ce03698bc404264350cd8
Bhavya Vardhan Jain <67510492+Bhavyavardhanjain@users.noreply.github.com>
2023-04-04 11:54:44 +0530

    Add files via upload

---
+++ Registration/main.php
@@ -0,0 +1,9 @@
+        <?php
+        error_reporting(0);
+        $connect=mysqli_connect("localhost","root","root","registration") or die(" Connection Failed");
+        $name=$_POST["name"];
+        if($name){
+            $query="insert into register values('$name')";
+            mysqli_query($connect,$query);
+        }
+        ?>
